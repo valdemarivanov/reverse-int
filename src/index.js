4a3118f6fddf48e8018a86dd86eb278800bbc7d7
valdemarivanov <79762542+valdemarivanov@users.noreply.github.com>
2021-03-10 15:54:50 +0200

    feat: upload index.js
    upload index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    if (n<0) n=-n;
+    if (n<10) return n;
+    let zifra;
+    let newn=0;
+    
+    while (n>9){
+         zifra=n%10;
+         n=(n-zifra)/10;
+         newn=newn*10+zifra;
+    }
+    newn=newn*10+n;
+    return newn
 }
